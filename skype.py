a2925ff572b7e170a01018121fd4d3e65fe9f26f
Adam Hitchcock <adam@northisup.com>
2010-05-13 15:25:14 -0700

    init

---
+++ skype.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+skype.py
+
+Created by Adam Hitchcock on 2010-05-13.
+Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+import foursquare
+import street_cred
+
+def main():
+    fs = foursquare.Foursquare(foursquare.BasicCredentials(username, password))
+    
+    
+
+
+if __name__ == '__main__':
+    main()
+
